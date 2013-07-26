a9a0dc974e8fbb715b8415608d47d99dc557cf31
Chris Murray <megachip90@hotmail.com>
2013-07-26 01:57:26 +0100

    Fix php warnings in paypal_payment.

--- crm/modules/paypal_payment/paypal_payment.inc.php
+++ crm/modules/paypal_payment/paypal_payment.inc.php
@@ -80,7 +80,9 @@ function paypal_payment_data_alter ($type, $data = array(), $opts = array()) {
             $paypal_payment_map = crm_map(crm_get_data('paypal_payment', $opts), 'pmtid');
             // Add paypal data to each payment data
             foreach ($data as $i => $payment) {
-                $data[$i]['paypal'] = $paypal_payment_map[$payment['pmtid']];
+                if (array_key_exists($payment['pmtid'], $paypal_payment_map)) {
+                    $data[$i]['paypal'] = $paypal_payment_map[$payment['pmtid']];
+                }
             }
     }
     return $data;
