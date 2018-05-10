453114c37885685d9292b92925ed39ec0adde2b5
Chris Murray <chris.f.murray@hotmail.co.uk>
2018-05-10 17:21:07 +0100

    update copyright year

--- crm/modules/register/register.inc.php
+++ crm/modules/register/register.inc.php
@@ -1,22 +1,22 @@
 <?php
 
 /*
-    Copyright 2009-2017 Edward L. Platt <ed@elplatt.com>
-    Copyright 2013-2017 Chris Murray <chris.f.murray@hotmail.co.uk>
+    Copyright 2009-2018 Edward L. Platt <ed@elplatt.com>
+    Copyright 2013-2018 Chris Murray <chris.f.murray@hotmail.co.uk>
     
     This file is part of the Seltzer CRM Project
     register.inc.php - registration module
-
+    
     Seltzer is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     any later version.
-
+    
     Seltzer is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
+    
     You should have received a copy of the GNU General Public License
     along with Seltzer.  If not, see <http://www.gnu.org/licenses/>.
 */
