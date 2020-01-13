9adf96046471c1db749c4c63babd7d0cf3c1ff27
Chris Murray <chris.f.murray@hotmail.co.uk>
2020-01-13 23:05:08 +0000

    Update copyright year #437
    Update copyright year #437

--- crm/modules/member/command.inc.php
+++ crm/modules/member/command.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-    Copyright 2009-2019 Edward L. Platt <ed@elplatt.com>
+    Copyright 2009-2020 Edward L. Platt <ed@elplatt.com>
     
     This file is part of the Seltzer CRM Project
     command.inc.php - Member module - request handlers
