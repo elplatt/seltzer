aab5ae07bd5d28cb394f529100489044f1f43976
Chris Murray <chris.f.murray@hotmail.co.uk>
2023-10-15 01:02:33 +0100

    add missing db_connect

--- crm/modules/member/data.inc.php
+++ crm/modules/member/data.inc.php
@@ -303,7 +303,7 @@ function member_save ($member) {
         ";
         $res = mysqli_query($db_connect, $sql);
         if (!$res) crm_error(mysqli_error($res));
-        if (mysqli_affected_rows() < 1) {
+        if (mysqli_affected_rows($db_connect) < 1) {
             return null;
         }
     }
