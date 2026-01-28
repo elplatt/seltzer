7bc66e78c6ccf08911094a8c141e848a1d54c8d6
Chris Murray <chris.f.murray@hotmail.co.uk>
2026-01-28 01:16:03 +0000

    Yearly copyright update

--- crm/include/page.inc.php
+++ crm/include/page.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-    Copyright 2009-2025 Edward L. Platt <ed@elplatt.com>
+    Copyright 2009-2026 Edward L. Platt <ed@elplatt.com>
     
     This file is part of the Seltzer CRM Project
     page.inc.php - Core pages
